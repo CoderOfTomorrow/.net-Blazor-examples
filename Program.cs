5591622e311ae3ce34de6a88e9bc0f0e1ccd9838
blackb0y999 <blackb0y999@yahoo.com>
2020-12-23 18:04:25 +0200

    //Removed useless things

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Test
+namespace blazor_Examples
 {
     public class Program
     {
