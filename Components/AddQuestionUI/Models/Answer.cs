d620f4561c6ec5abf6771ef0fdb958041e7b2262
blackb0y999 <blackb0y999@yahoo.com>
2020-12-30 16:36:44 +0200

    //Add Question UI

---
+++ Components/AddQuestionUI/Models/Answer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazor_Examples.Components.AddQuestionUI.Models
+{
+    public class Answer
+    {
+        public string Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ Components/AddQuestionUI/Models/Question.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazor_Examples.Components.AddQuestionUI.Models
+{
+    public class Question
+    {
+        public string Name { get; set; }
+        public List<Answer> Answers { get; set; }
+    }
+}
